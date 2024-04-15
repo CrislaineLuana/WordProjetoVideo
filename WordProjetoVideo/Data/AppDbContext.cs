64730767657bb1a8fae9690675e34b18533fd348
Crislaine <clpaula3007@gmail.com>
2024-04-15 09:46:24 -0300

    Adicionar arquivos de projeto.

---
+++ WordProjetoVideo/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WordProjetoVideo.Models;
+
+namespace WordProjetoVideo.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {            
+        }
+
+        public DbSet<Documento> Documentos { get; set; }
+
+    }
+}
